3ee6238e32db4cb743d32794a9b99e201f531533
Aaquib111 <aaquibahm@gmail.com>
2020-08-30 19:20:17 -0400

    Added angle test code

---
+++ ArduinoBLE/app/src/main/java/com/mitinventeam/arduinoble/MainActivity.java
@@ -0,0 +1,207 @@
+package com.mitinventeam.arduinoble;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+
+    private final String DEVICE_ADDRESS="64:69:4E:7B:12:53";
+    private UUID PORT_UUID;
+
+    Button startButton, sendButton,clearButton,stopButton;
+
+    boolean deviceConnected=false;
+
+    private OutputStream outputStream;
+    private InputStream inputStream;
+
+    TextView textView;
+    EditText editText;
+
+    Thread thread;
+    byte buffer[];
+    int bufferPosition;
+    boolean stopThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public boolean BTinit()
+    {
+        boolean found=false;
+        //Check if BLE is enabled on the phone
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+            Toast.makeText(this, "BLE not supported", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        //Set bluetooth manager and adapter
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        //Check if bluetooth is enabled
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 0);
+        }
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        Log.d("D", "Attempting to connect...");
+        boolean connected=true;
+        try {
+            PORT_UUID = UUID.fromString(device.getUuids()[0].toString());
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            Log.d("D", "Connected!");
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        return connected;
+    }
+
+    public void onClickStart(View view) {
+        Log.d("D", "Starting...");
+        if(BTinit())
+        {
+            if(BTconnect())
+            {
+                setUiEnabled(true);
+                deviceConnected=true;
+                beginListenForData();
+                textView.append("\nConnection Opened!\n");
+            }else{
+                Log.d("D", "BTConnect failed :(");
+            }
+
+        }else{
+            Log.d("D", "BTInit failed :(");
+        }
+    }
+
+    public void onClickStop(View view) {
+        stopThread = true;
+        try {
+            outputStream.close();
+            inputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setUiEnabled(false);
+        deviceConnected=false;
+        textView.append("\nConnection Closed!\n");
+    }
+
+    public void onClickSend(View view) {
+        String string = editText.getText().toString();
+        string.concat("\n");
+        try {
+            outputStream.write(string.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        textView.append("\nSent Data:"+string+"\n");
+
+    }
+
+    public void onClickClear(View view) {
+        textView.setText("");
+    }
+
+    public void setUiEnabled(boolean bool)
+    {
+        startButton.setEnabled(!bool);
+        sendButton.setEnabled(bool);
+        stopButton.setEnabled(bool);
+        textView.setEnabled(bool);
+
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        stopThread = false;
+        buffer = new byte[1024];
+        Thread thread  = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopThread)
+                {
+                    try
+                    {
+                        int byteCount = inputStream.available();
+                        if(byteCount > 0)
+                        {
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string=new String(rawBytes,"UTF-8");
+                            handler.post(new Runnable() {
+                                public void run()
+                                {
+                                    textView.append(string);
+                                }
+                            });
+
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopThread = true;
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+}
--- UsbSerial-6.1.0/usbserial/src/main/java/com/felhr/usbserial/SerialPortCallback.java
+++
@@ -1,8 +0,0 @@
-package com.felhr.usbserial;
-
-import java.util.List;
-
-
-public interface SerialPortCallback {
-    void onSerialPortsDetected(List<UsbSerialDevice> serialPorts);
-}
